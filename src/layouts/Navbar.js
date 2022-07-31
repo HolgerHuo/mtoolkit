5a46ad28830deb98fe83a5041e6d2313bbf869ce
Holger Huo <holgerhuo@outlook.com>
2022-07-31 13:18:27 +0800

    update: finalize nav-bar (it's still a mess but it works anyway)

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,139 +1,105 @@
-/* This navBar layout is pretty much unfinished.
-   To be improved
-*/
-
-import { useEffect, useState } from 'react';
-import { AppBar, Box, Container, Toolbar, Typography, IconButton, List, ListItem, ListItemButton, ListItemText, Drawer, Divider } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { AppBar, Box, Toolbar, Typography, IconButton, List, ListItem, ListItemButton, ListItemText, Drawer, Divider } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Link, NavLink } from 'react-router-dom';
+import InfoIcon from '@mui/icons-material/Info';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
-const navItems = [
+import {trackEvent} from '../utils/analyticsWrapper';
+import './Navbar.css';
+
+const drawerWidth = 240;
+
+const toolList = [
     { itemName: '微信接龙考勤', itemTo: '/check-list' }
 ];
 
-function Navbar(props) {
-    return (
-        <AppBar
-            position="fixed"
-            sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        >
-            <Toolbar>
-                <IconButton
-                    color="inherit"
-                    aria-label="open drawer"
-                    edge="start"
-                    onClick={props.handleDrawerToggle}
-                    sx={{ mr: 2 }}
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" component={NavLink} sx={{ fontFamily: 'Permanent Marker', textAlign: 'left', textDecoration: 'none', color: 'unset', fontWeight: 600, marginRight: '15px' }} to='/'>
-                    MToolkit
-                </Typography>
-                <Typography variant="subtitle1" sx={{ textAlign: 'left' }} to='/'>
-                    全能的班长工具箱
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    );
-}
-
-function Sidebar(props) {
-    const drawer = (
-        <div>
-            <Toolbar />
-            <Divider />
-            <List>
-                {navItems.map((i) => (
-                    <ListItem key={i.itemName} disablePadding>
-                        <ListItemButton sx={{ textAlign: 'center' }} component={Link} to={i.itemTo} onClick={props.onClick} >
-                            <ListItemText primary={i.itemName} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-        </div>
-    );
-
-    return (
-        <Box
-            component="nav"
-            sx={{ width: { sm: props.drawerWidth }, flexShrink: { sm: 0 } }}
-        >
-            <Drawer
-                variant="persistent"
-                sx={{
-                    width: props.drawerWidth,
-                    flexShrink: 0,
-                    '& .MuiDrawer-paper': {
-                        width: props.drawerWidth,
-                        boxSizing: 'border-box',
-                    },
-                }}
-                open={props.open}
-                onClose={props.handleDrawerToggle}
-                ModalProps={{
-                    keepMounted: true,
-                }}
-            >
-                {drawer}
-            </Drawer>
-        </Box>
-    )
-}
-
-export default function NavFrame(props) {
+export default function NavFrame() {
     const [drawerOpen, setDrawerOpen] = useState(false);
 
-    const handleDrawerToggle = () => {
-        setDrawerOpen(!drawerOpen);
-    };
-
-    const handleListClick = () => {
-        if (window.innerWidth < 600) {
-            handleDrawerToggle()
-        }
-    }
-
-    const drawerWidth = 240;
-
-    const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-        ({ theme, open }) => ({
-            flexGrow: 1,
-            padding: theme.spacing(3),
-            transition: theme.transitions.create('margin', {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.leavingScreen,
-            }),
-            marginLeft: `-${drawerWidth}px`,
-            ...(open && {
-                transition: theme.transitions.create('margin', {
-                    easing: theme.transitions.easing.easeOut,
-                    duration: theme.transitions.duration.enteringScreen,
-                }),
-                marginLeft: 0,
-            }),
-        }),
-    );
-
-    useEffect(() => {
-        if (window.innerWidth >= 600) {
-            setDrawerOpen(!drawerOpen);
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    const navigate = useNavigate();
 
     return (
         <>
-            <Navbar open={drawerOpen} handleDrawerToggle={handleDrawerToggle} />
-            <Sidebar open={drawerOpen} handleDrawerToggle={handleDrawerToggle} drawerWidth={drawerWidth} onClick={handleListClick} />
-            <Main open={drawerOpen}>
-                <Container maxWidth='sm'>
-                    <Toolbar />
-                    {props.children}
-                </Container>
-            </Main>
+            <AppBar
+                position="fixed"
+                sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={() => setDrawerOpen(!drawerOpen)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component={NavLink} sx={{ fontFamily: 'Permanent Marker', textAlign: 'left', textDecoration: 'none', color: 'unset', fontWeight: 600, marginRight: '15px', ml: 2 }} to='/' onClick={() => setDrawerOpen(false)}>
+                        MToolkit
+                    </Typography>
+                    <Typography variant="subtitle1" sx={{ textAlign: 'left', mt: 0.5, flexGrow: 1}} className='subtitle'>
+                        全能的班长工具箱
+                    </Typography>
+                    <IconButton
+                        color="inherit"
+                        aria-label="gitHub link"
+                        onClick={() => {
+                            trackEvent('github', 'click');
+                            setDrawerOpen(false);
+                            }
+                        }
+                        href='https://github.com/holgerhuo/mtoolkit'
+                        target='_blank'
+                    >
+                        <GitHubIcon />
+                    </IconButton>
+                    <IconButton
+                        color="inherit"
+                        aria-label="go to about page"
+                        onClick={() => {
+                            setDrawerOpen(false);
+                            navigate("/about");
+                            }
+                        }
+                    >
+                        <InfoIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Box
+                component="nav"
+                sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+            >
+                <Drawer
+                    variant="persistent"
+                    sx={{
+                        width: drawerWidth,
+                        flexShrink: 0,
+                        '& .MuiDrawer-paper': {
+                            width: drawerWidth,
+                            boxSizing: 'border-box',
+                        },
+                    }}
+                    open={drawerOpen}
+                    onClose={() => setDrawerOpen(false)}
+                    ModalProps={{
+                        keepMounted: true,
+                    }}
+                >
+                    <div className='sidebar-items'>
+                        <Divider />
+                        <List>
+                            {toolList.map((i) => (
+                                <ListItem key={i.itemName} disablePadding>
+                                    <ListItemButton sx={{ textAlign: 'center' }} component={NavLink} to={i.itemTo} onClick={() => setDrawerOpen(!drawerOpen)} >
+                                        <ListItemText primary={i.itemName} />
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </div>
+                </Drawer>
+            </Box>
         </>
     )
 }
